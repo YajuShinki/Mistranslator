49f21a79f7c54c71eedf6ba3f054bd73d2f305f9
YajuShinki <yajuukamiki@gmail.com>
2020-12-06 00:10:17 -0500

    Removed unnecessary file

--- discord-init.py
+++ discord-init.py
@@ -13,7 +13,12 @@ class BotClient(discord.Client):
         if message.author == client.user:
             return
         #Parse input
-        if message.content.startswith('mistl!'):
+        if message.content.startswith('mistl:'):
+            #Strip invocation from input message
+            inputstr = message.content[6:]
+            command = inputstr.split(' ',1)[0]
+            inputstr = inputstr.split(' ',1)[1]
+            
             
         
 client = BotClient()
--- gct-init.py
+++
@@ -1,9 +0,0 @@
-import google.cloud.translate_v2 as tl
-import sys,os
-
-#set environment variable
-__location__ = os.path.join(os.getcwd(),os.path.dirname(__file__))
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.join(__location, 'gct-key.json')
-
-tlclient = tl.Client()
-
