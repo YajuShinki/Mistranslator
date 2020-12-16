d0fd1d43a3ac922dc1ebdfc92ed72af8a97e7d2d
YajuShinki <yajuukamiki@gmail.com>
2020-12-16 01:01:17 -0500

    Added extra data to mistranslate output

--- discord-init.py
+++ discord-init.py
@@ -63,8 +63,9 @@ async def send_result(ctx,resultinfo,verbose=False,dump=False):
     embed = discord.Embed(title='Translation Result',colour=0x00ff3f)
     embed.add_field(name='Input',value=resultinfo['input']) 
     if verbose:
+        
         embed.add_field(name='Input Language',value=f'{resultinfo['inputlangname']} (`{resultinfo['inputlang']}`)')
-        embed.add_field(name='Iterations',value=len(resultinfo['iters'])-1)
+        embed.add_field(name='Iterations',value=f'{len(resultinfo['iters'])}'-1)
         embed.add_field(name='Output Language',value=resultinfo['iters'][-1]['language'])
     embed.add_field(name='Output',value=resultinfo['iters'][-1]['result'])
     await ctx.send(ctx.author.mention,embed=embed,file=rfile)
